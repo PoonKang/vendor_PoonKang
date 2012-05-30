a7150eb928635617718b14c2eea062edcd92addf
PoonKang <PoonKang@PoonKang.com>
2012-05-30 13:23:27 -0400

    Vendor tag

--- products/fascinatemtd.mk
+++ products/fascinatemtd.mk
@@ -17,6 +17,7 @@ PRODUCT_DEVICE := fascinatemtd
 PRODUCT_NAME := PoonKang_fascinatemtd
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SCH-I500
+PRODUCT_VENDOR := PoonKang
 
 #Set build fingerprint / ID / Prduct Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SCH-I500 TARGET_DEVICE=SCH-I500 BUILD_ID=IML74K BUILD_FINGERPRINT=google/soju/crespo:2.3.4/GRJ22/121341:user/release-keys PRIVATE_BUILD_DESC="soju-user 2.3.4 GRJ22 121341 release-keys"
