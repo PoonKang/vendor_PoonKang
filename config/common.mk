a68ecd17dabd9497cf590b823244e4a9074e2419
PoonKang <PoonKangGit@gmail.com>
2013-07-09 13:34:05 -0400

    Consolidating from common_full_phone

--- config/common.mk
+++ config/common.mk
@@ -14,5 +14,26 @@ endif
 
 # Define JellyBeer Target
 ifeq ($(BUILD_TYPE),JellyBeer)
+
+# Bring in all audio files
+include frameworks/base/data/sounds/NewAudio.mk
+
+# Extra Ringtones
+include frameworks/base/data/sounds/AudioPackageNewWave.mk
+
+# Include CM audio files
+include vendor/cm/config/cm_audio.mk
+
+# Optional CM packages
+PRODUCT_PACKAGES += \
+    HoloSpiralWallpaper \
+    MagicSmokeWallpapers \
+    NoiseField \
+    Galaxy4 \
+    LiveWallpapers \
+    LiveWallpapersPicker \
+    VisualizationWallpapers \
+    PhaseBeam
+
 -include vendor/PoonKang/config/jellybeer.mk
 endif
