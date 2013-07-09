5b42da41e05aecac3e7c5d188efda22bb5336418
PoonKang <PoonKangGit@gmail.com>
2013-07-09 12:51:43 -0400

    Remove old configs

--- configs/common_phone.mk
+++
@@ -1,7 +0,0 @@
-$(call inherit-product, vendor/PoonKang/configs/common.mk)
-
-# PRODUCT_COPY_FILES += \
-#    vendor/aokp/prebuilt/common/app/NovaLauncher.apk:system/app/NovaLauncher.apk
-     
-    
-    
