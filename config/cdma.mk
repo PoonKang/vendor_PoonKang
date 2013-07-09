72135413990097759c2f002a8aa87b041c59fbe6
PoonKang <PoonKangGit@gmail.com>
2013-07-09 12:53:55 -0400

    Remove telephony makes

--- config/cdma.mk
+++
@@ -1,4 +0,0 @@
-# CDMA APN list
-PRODUCT_COPY_FILES += \
-    vendor/PoonKang/prebuilt/common/etc/apns-conf-cdma.xml:system/etc/apns-conf.xml \
-    frameworks/base/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml
