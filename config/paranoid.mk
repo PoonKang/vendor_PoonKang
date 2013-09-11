bab54c33af0033773945408fba66284097bc0ee0
PoonKang <PoonKangGit@gmail.com>
2013-09-11 10:47:48 -0400

    Add Theme Engine  * cleanup

--- config/paranoid.mk
+++ config/paranoid.mk
@@ -23,6 +23,9 @@ PRODUCT_COPY_FILES += \
     vendor/pa/prebuilt/common/bin/backuptool.functions:system/bin/backuptool.functions \
     vendor/pa/prebuilt/common/bin/50-backupScript.sh:system/addon.d/50-backupScript.sh
 
+# T-Mobile theme engine
+include vendor/pa/packages/theme_engine.mk
+
 # Gesture enabled JNI
 PRODUCT_COPY_FILES += \
     vendor/pa/prebuilt/common/lib/libjni_latinime.so:system/lib/libjni_latinime.so
@@ -38,12 +41,6 @@ else
     # Build paprefs from sources
     PRODUCT_PACKAGES += \
         ParanoidPreferences
-endif
-
-# ParanoidOTA
-ifneq ($(NO_OTA_BUILD),true)
-    PRODUCT_PACKAGES += \
-        ParanoidOTA
 endif
 
 ifneq ($(PARANOID_BOOTANIMATION_NAME),)
