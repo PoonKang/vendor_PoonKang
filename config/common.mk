738c7d88a2413cc9d62de8212cffa49412c05a53
PoonKang <PoonKangGit@gmail.com>
2013-09-11 11:18:08 -0400

    Revert "Initial PAC-man support"
    This reverts commit 10584d23ec15fc0120d0b14df42c1d17063c7600.
    
    Notes:
    I rarely use these, just makes clutter...
    Perhaps refactor and branch out!

--- config/common.mk
+++ config/common.mk
@@ -53,9 +53,3 @@ ifeq ($(BUILD_TYPE),Carbon)
 -include vendor/PoonKang/config/carbon.mk
 -include vendor/PoonKang/config/staging.mk
 endif
-
-# Define PAC-man
-ifeq ($(BUILD_TYPE),PACMAN)
--include vendor/PoonKang/config/pac-man.mk
--include vendor/PoonKang/config/staging.mk
-endif
